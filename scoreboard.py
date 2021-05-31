c503df078c57a852dee08e5af92959bd4103cb17
Klaud <79310242+KelzaPrograms@users.noreply.github.com>
2021-05-31 18:31:57 +0100

    Added high score to the game!
    So you can keep track of how amazing you are.

--- scoreboard.py
+++ scoreboard.py
@@ -5,6 +5,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -12,13 +14,17 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align="center", font=("Courier", 20, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align="center", font=("Courier", 20, "normal"))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over.", align="center", font=("Courier", 20, "normal"))
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_score()
 
     def point(self):
         self.score += 1
-        self.clear()
         self.update_score()
